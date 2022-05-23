efc60c15c892daae7fc65b25a0d4d42f2ad3cd1c
miotholerus <mio.tholerus@stud.sti.se>
2022-05-23 11:21:32 +0200

    Added DB constraint - hostname must be unique

--- src/main/java/com/tholerus/clockmancerrest/model/Domain.java
+++ src/main/java/com/tholerus/clockmancerrest/model/Domain.java
@@ -11,6 +11,7 @@ public class Domain {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private String id;
+    @Column(unique=true)
     private String hostname;
     private boolean limited;
     @OneToMany
